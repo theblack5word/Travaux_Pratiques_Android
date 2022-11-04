1bc8ca9f5fc3e3ad453172b4c24a0d799345bfd4
theblack5word <103179789+theblack5word@users.noreply.github.com>
2022-11-04 14:47:27 +0100

    module 5 tp link couleurs corrigé

--- mod5navtp/src/main/java/com/eni/myapplication/OrangeFragment.kt
+++ mod5navtp/src/main/java/com/eni/myapplication/OrangeFragment.kt
@@ -1,10 +1,12 @@
 package com.eni.myapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 
 class OrangeFragment : Fragment() {
@@ -16,5 +18,11 @@ class OrangeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_orange, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonOrangeToStart = view.findViewById<Button>(R.id.buttonOrangeToStart)
+        buttonOrangeToStart.setOnClickListener{
+            Navigation.findNavController(view).popBackStack(R.id.whiteFragment,false)
+        }
+    }
 }
